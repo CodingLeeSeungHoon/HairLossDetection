90cb18afda0210c1880d4bdfbcd5e48a0e430b89
Y-greatigr <yhb1427@gmail.com>
2022-02-07 20:47:52 +0900

    [JBMB-24] jbmb-coreserver 생성 및 개발환경 구성 (이한범)

---
+++ jbmb_coreserver/src/test/java/com/jbmb/jbmb_coreserver/JbmbCoreserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbmb.jbmb_coreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbmbCoreserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
