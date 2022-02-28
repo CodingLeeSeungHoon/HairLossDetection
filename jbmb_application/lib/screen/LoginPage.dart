94ced113feda2bea146588f5b7c0aedccfc3bb1a
CodingLeeSeungHoon <free_minkya@naver.com>
2022-02-28 21:52:47 +0900

    [JBMB-34] LoginPage UI Dev

--- jbmb_application/lib/screen/LoginPage.dart
+++ jbmb_application/lib/screen/LoginPage.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:jbmb_application/widget/ClickableButton.dart';
 
 class LoginPage extends StatelessWidget {
+  final String jbmbLogoPath = 'images/jbmb_logo.png';
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("loginPage"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(jbmbLogoPath),
+              const SizedBox(height: 30.0,),
+              const SizedBox(
+                width: 342.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'ID',
+                    hintText: 'Enter your ID',
+                    labelStyle: TextStyle(color: Colors.black),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(width: 1, color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(width: 1, color: Colors.black),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+              const SizedBox(height: 15.0,),
+              const SizedBox(
+                width: 342.0,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'PW',
+                    hintText: 'Enter your PW',
+                    labelStyle: TextStyle(color: Colors.black),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(width: 1, color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(width: 1, color: Colors.black),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+              const SizedBox(height: 15.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ClickableButton(buttonText: '로그인', iconData: Icons.login, onPressed: (){},),
+                  ClickableButton(buttonText: '회원가입', iconData: Icons.account_box, onPressed: (){},),
+                ],
+              )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
