668948b9e096f06d766f8a86b20d0f7242f34f5c
Y-greatigr <yhb1427@gmail.com>
2022-03-23 02:13:35 +0900

    [JBMB-49] 회원정보 가져오기 (이한범)

---
+++ jbmb_coreserver/src/main/java/com/jbmb/jbmb_coreserver/account/dto/Information.java
@@ -0,0 +1,20 @@
+package com.jbmb.jbmb_coreserver.account.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Information {
+    private Integer resultCode;
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private Integer sex;
+    private Integer age;
+    private Integer hairType;
+}
